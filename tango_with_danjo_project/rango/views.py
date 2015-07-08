daf3c2fb1080a2a44f0a41de4660bd0715a5a24a
Avinash <avinashdevicode@gmail.com>
2015-07-08 22:31:13 +0530

    c

--- tango_with_danjo_project/rango/views.py
+++ tango_with_danjo_project/rango/views.py
@@ -5,7 +5,22 @@ from rango.models import Category, Page
 def index(request):
     categories = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories':categories}
-    return render(request, 'rango/index.html', context_dict) 
+    return render(request, 'rango/index.html', context_dict)
+
+def category(request, category_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_slug)
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category = category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+        
+    except Category.doesNotExist:
+        pass
+    
+    return render(request, 'rango/category.html', context_dict)
+        
   
 def about(request):
     context_dict = {'message':"this is about page hee"}
